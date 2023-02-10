088f67dd506b8a49f6176fa47fff4c5d8fd80bf0
MaksimS <sipatovGT@mail.ru>
2023-02-10 14:54:32 +0400

    seminar1

---
+++ HW_Seminar1/Main.java
@@ -0,0 +1,28 @@
+package HW_Seminar1;
+
+    public class Main {
+
+    public static void main(String[] args) {
+        int a = (int) (Math.random()* 2000);
+       
+            System.out.println(a);
+ 
+    String binary = Long.toBinaryString(a);
+ 
+    System.out.println(binary);
+
+    int n = binary.length() - 1;
+     System.out.println(n);
+    
+    int min = Short.MIN_VALUE;
+    int max = Short.MAX_VALUE;
+    int result;
+
+    for (int i = a; i < max; i ++){
+        if (i % n == 0);
+        result=+1;
+    System.out.println(result);
+    }
+    
+    }
+}
