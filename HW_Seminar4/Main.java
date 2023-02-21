f1707a36a2166b889d54ed194cc7b75cb15ad679
MaksimS <sipatovGT@mail.ru>
2023-02-21 17:23:36 +0400

    startDZ_S4

--- HW_Seminar4/Main.java
+++ HW_Seminar4/Main.java
@@ -5,19 +5,19 @@ import java.io.*;
 
 public class Main {
     
-    public static void main(String[] args) throws EOFException {
+    public static void main(String[] args) throws Exception {
 
     ArrayList<String[]> list = new ArrayList<>();
 
     // Scanner scanner = new Scanner(System.in, "ibm866");
     // System.out.println("ФИО возраст и пол (Выход: x): ");
 
-    BufferedReader str = new BufferedReader(new FileReader("users.txt"));
+    BufferedReader br = new BufferedReader(new FileReader("users.txt"));
     String str;
     while((str = br.readLine()) != null){
         System.out.printf("== %s ==\n", str);
     }
-    str.close();
+    br.close();
 
     // boolean run = true;
     // while (run){
