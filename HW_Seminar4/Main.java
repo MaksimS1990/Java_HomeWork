d69683ad1c62a92a8f048d7e103f4857dd199ca5
MaksimS <sipatovgt@mail.ru>
2023-02-22 20:45:01 +0400

    endHW_Seminar4

--- HW_Seminar4/Main.java
+++ HW_Seminar4/Main.java
@@ -4,48 +4,88 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    public static void BubbleSortAge(ArrayList <Integer> age, ArrayList<Integer> index) {
+
+        ArrayList<Integer> ageTmp = new ArrayList<>(age);
+
+        boolean sort = true;
+        int k = ageTmp.size();
+        while (sort) {
+            sort = false;
+            for (int i = 0; i < k - 1; i++) {
+                if(ageTmp.get(i) > ageTmp.get(i + 1)){
+                    int temp = ageTmp.get(i);
+                    ageTmp.set(i, ageTmp.get(i + 1));
+                    ageTmp.set(i + 1, temp);
+
+                    int temp2 = index.get(i);
+                    index.set(i, index.get(i + 1));
+                    index.set(i + 1, temp2);
+                    sort = true;
+                }
+            }
+            k--;
+        }
+    }
     
     public static void main(String[] args) throws Exception {
 
-    ArrayList<String> lastname = new ArrayList<>();
-    ArrayList<String> firstname = new ArrayList<>();
-    ArrayList<String> patronymic = new ArrayList<>();
-    ArrayList<Integer> age = new ArrayList<>();
-    ArrayList<String> gender = new ArrayList<>();
+    ArrayList<Integer> index = new ArrayList<>();
+    ArrayList <String> lastname = new ArrayList<>();
+    ArrayList <String> firstname = new ArrayList<>();
+    ArrayList <String> patronymic = new ArrayList<>();
+    ArrayList <Integer> age = new ArrayList<>();
+    ArrayList <String> gender = new ArrayList<>();
 
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in, "cp866");
     System.out.println("ФИО возраст пол, через пробел(Выход x): ");
-    System.out.println();
 
-    BufferedReader br = new BufferedReader(new FileReader("users.txt"));
-    String str;
-    while((str = br.readLine()) != null){
-        System.out.printf("%s \n", str);
-    }
-    br.close();
-
-    boolean run  = true;
-    while(run) {
+    boolean run = true;
+    while (run) {
         String in = scanner.nextLine();
-        if (in.equals("x")) {
+        if(in.equals("x")){
             run = false;
         }
         else {
-            String [] users = in.split(" ");
-            lastname.add(users[0]);
-            firstname.add(users[1]);
-            patronymic.add(users[2]);
-            age.add(Integer.parseInt(users[3]));
-            gender.add(users[4]);
+            String [] data = in.split(" ");
+            index.add(lastname.size());
+            lastname.add(data[0]);
+            firstname.add(data[1]);
+            patronymic.add(data[2]);
+            age.add(Integer.parseInt(data[3]));
+            gender.add(data[4]);
         }
     }
-     
-    for(int i = 0; i < lastname.size(); i++){
+        System.out.println("Вывод списка: ");
+        for(int i = 0; i < lastname.size(); i++){
         StringBuilder st = new StringBuilder();
-        st.append(lastname.get(i)).append(" ").append(firstname.get(i).charAt(0)).append(". ").append(patronymic.get(i).charAt(0));
-    System.out.println(st);
+         st.append(lastname.get(i))
+            .append(" ")
+            .append(firstname.get(i)
+            .charAt(0))
+            .append(". ")
+            .append(patronymic.get(i)
+            .charAt(0))
+            .append(". ");
+        System.out.println(st);
     }
+
+    BubbleSortAge(age, index);
+
+    System.out.println("Вывод отсортированного списка: ");
+    for(int i: index) {
+        StringBuilder st = new StringBuilder();
+         st.append(lastname.get(i))
+            .append(" ")
+            .append(firstname.get(i)
+            .charAt(0))
+            .append(". ")
+            .append(patronymic.get(i)
+            .charAt(0))
+            .append(". ");
+        System.out.println(st);
     }
+
 }
-
-
+}
