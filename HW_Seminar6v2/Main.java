1a3195cd83314157b2a6518bc873591e6d22d140
MaksimS <sipatovGT@mail.ru>
2023-02-28 17:17:18 +0400

    completeDZ6v2

---
+++ HW_Seminar6v2/Main.java
@@ -0,0 +1,129 @@
+package HW_Seminar6v2;
+
+import java.util.ArrayList;
+//import java.util.ArrayList;
+import java.util.HashMap;
+//import java.util.HashSet;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        MySet set = new MySet();
+        set.add(2);
+        set.add(33);
+        set.add(22);
+        set.add(41);
+        set.add(67);
+        set.add(28);
+        set.add(77);
+        System.out.println(set);
+        System.out.println(set.contains(33));
+        System.out.println(set.contains(59));
+        set.clear();
+        System.out.println(set);
+        set.add(2);
+        set.add(36);
+        set.add(22);
+        set.add(41);
+        set.add(6);
+        set.add(28);
+        set.add(16);
+        System.out.println(set);
+        set.remove(41);
+        System.out.println(set);
+        System.out.println(set.size());
+        System.out.println(set.get(2));
+        System.out.println(set.getArrayList());
+        System.out.println(set.headSet(22));
+        System.out.println(set.tailSet(22));
+        System.out.println(set.subSet(2, 6));
+
+    }
+    }
+
+
+class MySet{
+    private HashMap<Integer, Object> values = new HashMap<>();
+    private static final Object X = new Object();
+
+    void add (int data){
+        values.put(data, X);
+        }
+
+    public String toString() {
+        return values.keySet().toString();
+    }
+
+    Boolean contains (int data){
+        return values.containsKey(data);
+    }
+
+    void clear (){
+        values.clear();
+    }
+
+    void remove (int data){
+        values.remove(data);
+    }
+
+    int size (){
+        return values.size();
+    }
+
+    int get (int ind){
+        return (int) values.keySet().toArray()[ind];
+    }
+
+    ArrayList getArrayList(){
+        ArrayList <Integer> res = new ArrayList<>();
+        values.forEach((k, v) -> res.add(k));
+        return res;
+    }
+
+    MySet tailSet(int data){
+        boolean ad = false;
+        MySet res = new MySet();
+        for (int el : values.keySet()) {
+            if (data == el)
+                ad = true;
+            if (ad){
+                res.add(el);
+            }
+        }
+        return res;
+    }
+
+        MySet headSet(int data){
+            MySet res = new MySet();
+            for (int el : values.keySet()) {
+                res.add(el);
+                if (data == el)
+                break;
+            }
+            return res;
+        }
+
+        MySet subSet(int key1, int key2){
+            MySet res = new MySet();
+            boolean ad = false;
+            int stop = key1;
+            for (int el : values.keySet()) {
+                if (el == key1 && !ad){
+                    ad = true;
+                    stop = key2;
+                }
+                if (el == key2 && !ad){
+                    ad = true;
+                }
+                    if (ad){
+                        res.add(el);
+                        if(el == stop){
+                            break;
+                        }
+                    }
+                }
+                return res;
+            }
+        }
+
